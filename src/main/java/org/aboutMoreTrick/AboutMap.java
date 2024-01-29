7ec3d60d021aaad452154e5e1082a65d1e50f3c3
“Ayin” <564375197@qq.com>
2024-01-29 17:41:00 +0800

    补充关于hashMap的算法题

--- src/main/java/org/aboutMoreTrick/AboutMap.java
+++ src/main/java/org/aboutMoreTrick/AboutMap.java
@@ -1,9 +1,6 @@
 package org.aboutMoreTrick;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * @author Ayin
@@ -54,5 +51,46 @@ public class AboutMap {
         //合并两个HashMap。可以使用putAll方法--遵循key唯一。相同则覆盖的逻辑
         map.putAll(map2);
 
+    }
+    /**
+     * leetcode:49.字母异位词分组
+     * */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map=new HashMap();
+        for (int i = 0; i < strs.length; i++) {
+            // 将字符串转换为字符数组，进行排序，再转换为字符串
+            char[] charArray = strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            // 如果排序后的字符串不在map中，创建一个新的列表
+            if (!map.containsKey(sortedStr)) {
+                map.put(sortedStr, new ArrayList<>());
+            }
+            // 将原始字符串添加到对应的列表中
+            map.get(sortedStr).add(strs[i]);
+        }
+        // 将map的值转换为列表，得到分组好的异位词列表
+        List<List<String>> result = new ArrayList<>(map.values());
+        return result;
+    }
+    /**
+     * leetcode:1.两数之和
+     * */
+    public int[] twonum(int[] nums, int target){
+        int[] res = new int[2];
+        if(nums == null || nums.length == 0){
+            return res;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int temp = target - nums[i];   // 遍历当前元素，并在map中寻找是否有匹配的key
+            if(map.containsKey(temp)){
+                res[1] = i;
+                res[0] = map.get(temp);
+                break;
+            }
+            map.put(nums[i], i);    // 如果没找到匹配对，就把访问过的元素和下标加入到map中
+        }
+        return res;
     }
 }
